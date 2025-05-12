d0a049caec6ac8160c4a86059ae90fb785225257
Olivia Fishbough <122580039+ittybittyfishy@users.noreply.github.com>
2025-05-12 16:32:32 -0700

    Added bar and pie charts

--- RedditScrapper.py
+++ RedditScrapper.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import praw
 from textblob import TextBlob
 from collections import Counter
+import matplotlib.pyplot as plt
+
 
 load_dotenv()  # Load Reddit project inforamtion from .env
 
@@ -33,33 +35,116 @@ def analyze_sentiment(text):
     else:
         return "Neutral"
 
-# Holds resulting sentiments 
-sentiments = []
+def draw_pie_chart(keyword, subreddit_name, sizes, labels):
 
-# User input to get results
-subreddit_name = input("Enter subreddit (or 'all'): ")
-keyword = input("Enter search keyword: ")
-limit = int(input("How many posts? "))
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%',startangle=140)
+    plt.axis('equal') # Make it a circle
+    plt.title(f"Sentiment Analysis for '{keyword}' in r/{subreddit_name}")
+    plt.show()
 
-# Ensure subreddit exists
-subreddit = reddit.subreddit(subreddit_name)
+def draw_bar_chart(keyword, subreddit_name, sizes, labels):
 
-# Analyze title and content of post and assign sentimanet score
-for post in subreddit.search(keyword, limit=limit):
-    if post.selftext:
-        content = f"{post.title}\n{post.selftext}"
-    else: 
-        content = post.title
-    label = analyze_sentiment(content)
-    sentiments.append((content, label))
+    plt.bar(labels, sizes)
+    plt.xticks(rotation=30)
+    plt.xlabel("Sentiment")
+    plt.ylabel("Number of Posts/Comments")
+    plt.title(f"Sentiment Analysis for '{keyword}' in r/{subreddit_name}")
+    plt.tight_layout()
+    plt.show()
 
-for comment in subreddit.comments(limit=limit):
-    if keyword.lower() in comment.body.lower():
-        label = analyze_sentiment(comment.body)
-        sentiments.append((comment.body, label))
+def main():
 
-for text, label in sentiments:
-    print(f"\n[{label}]\n{text[:300]}...\n")
+    newSearch = True
 
-sentiment_counts = Counter([label for _, label in sentiments])
-print(sentiment_counts)
+    while newSearch:
+        # Holds resulting sentiments 
+        sentiments = []
+
+        # User input to get results
+        subreddit_name = input("Enter subreddit (or 'all'): ")
+        keyword = input("Enter search keyword: ")
+        limit = int(input("How many posts? "))
+
+        # Ensure subreddit exists
+        subreddit = reddit.subreddit(subreddit_name)
+
+        # Analyze title and content of post and assign sentimanet score
+        for post in subreddit.search(keyword, limit=limit):
+            # Ensure that the post body is not empty
+            if post.selftext:
+                content = f"{post.title}\n{post.selftext}"
+            else: 
+                # If the post body is empty, only analyze post title
+                content = post.title
+            label = analyze_sentiment(content)
+            sentiments.append((content, label))
+
+        # Analyze reddit post comments 
+        for comment in subreddit.comments(limit=limit):
+            if keyword.lower() in comment.body.lower():
+                label = analyze_sentiment(comment.body)
+                sentiments.append((comment.body, label))
+
+        # for text, label in sentiments:
+        #     print(f"\n[{label}]\n{text[:300]}...\n")
+
+        # Count number of each label 
+        sentiment_counts = Counter([label for _, label in sentiments])
+
+        # Sort the labels
+        ordered_labels = [
+            "Very Positive",
+            "Positive",
+            "Slightly Positive",
+            "Neutral",
+            "Slightly Negative",
+            "Negative",
+            "Very Negative"
+        ]
+
+        labels = []
+        sizes = []
+
+        for label in ordered_labels:
+            if label in sentiment_counts:
+                labels.append(label)
+                sizes.append(sentiment_counts[label])
+
+        # Print the sentiment analysis
+        print(sentiment_counts)
+
+        # Ask user if they would like to see the data put into a graph 
+        chooseGraph = ""
+
+        while chooseGraph not in ["1","2","3"]:
+            chooseGraph = input("Would you like to see the inforion displayed as: \n1. Bar graph\n2. Pie chart\n3. New search\n")
+        
+        # Load graph the user asks for
+        match chooseGraph:
+            case "1":
+                # Bar graph
+                draw_bar_chart(keyword, subreddit_name, sizes, labels)
+                otherGraph = input("Would you like to see the pie graph? (Yes/No)")
+                if otherGraph.lower() == 'yes':
+                    draw_pie_chart(keyword, subreddit_name, sizes, labels)
+                else:
+                    continue
+            
+            case "2":
+                # pie chart
+                draw_pie_chart(keyword, subreddit_name, sizes, labels)
+                otherGraph = input("Would you like to see the bar graph? (Yes/No)")
+                if otherGraph.lower() == 'yes':
+                    draw_bar_chart(keyword, subreddit_name, sizes, labels)
+                else:
+                    continue
+            case "3":
+                # New Search
+                continue
+        
+        # Ask user if they would lilke to do a new search
+        nextSearch = input("Would you like to do a new search? (Yes/No)")
+        if nextSearch.lower() != 'yes':
+            newSearch = False
+
+main()
